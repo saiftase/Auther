6b3b8de981d4444d3efb5ca0e716ee61dc3c4244
Saif Tase <saiftase@gmail.com>
2016-04-07 20:12:55 -0400

    Serialize User from Google

--- server/app/index.js
+++ server/app/index.js
@@ -8,10 +8,15 @@ var chalk = require('chalk');
 // app.use(require('body-parser').json);
 app.use(require('body-parser').urlencoded({extended: false}));
 
+//General Middleware
 app.use(require('./logging.middleware'));
 app.use(require('./requestState.middleware'));
 app.use(require('./statics.middleware'));
 
+//Passport
+app.use(require('./passport.middleware'))
+
+//Secrets + API
 app.use(session({ secret: 'tongiscool' }));
 app.use('/api', require('../api/api.router'));
 
@@ -20,9 +25,6 @@ app.use(function (req, res, next) {
     console.log('session', req.session);
     next();
 });
-
-//Passport
-app.use(require('./passport.middleware'))
 
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
--- server/app/passport.middleware.js
+++ server/app/passport.middleware.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var router = require('express').Router();
+var User = require('../api/users/user.model');
 
 router.use(passport.initialize());
 router.use(passport.session());
@@ -15,9 +16,23 @@ passport.use(
   function (token, refreshToken, profile, done) {
     // the callback will pass back user profile information and each service (Facebook, Twitter, and Google) will pass it back a different way. Passport standardizes the information that comes back in its profile object.
     console.log('---', 'in verification callback', profile, '---');
-    done();
-   
+    passport.serializeUser(profile, done);  
   })
 );
+
+passport.serializeUser(function (user, done) {
+  User.findOne({
+    _id: user
+  })
+  .then(function(reponse){
+    console.log("RESPONSible", reponse);
+  })
+
+  done(null, user);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, done);
+});
 
 module.exports = router
